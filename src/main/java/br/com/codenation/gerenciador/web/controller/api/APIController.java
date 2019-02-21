07432d1c9435baec95f08d9a5d1d6ac3662b21b5
Samuel Rocha <samuelrodriguessr@hotmail.com>
2019-02-21 08:20:01 -0300

    add ControllerAPI Interface

---
+++ src/main/java/br/com/codenation/gerenciador/web/controller/api/APIController.java
@@ -0,0 +1,13 @@
+package br.com.codenation.gerenciador.web.controller.api;
+
+import java.util.List;
+
+public interface APIController<T> {
+
+    List<T> buscaTodos();
+    T  buscaPorId(String id);
+    void remove();
+    T adiciona(T t);
+    T atualiza(String id, T t);
+
+}
